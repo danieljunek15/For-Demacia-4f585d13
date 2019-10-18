684bb5e8068cd304af72122b778cb4f6dfb265be
danieljunek15 <dvandenbrink@roc-dev.com>
2019-10-18 14:02:53 +0200

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,20 @@
+<?php
+$name = array(
+    "joe",
+    "mama",
+    "pizza",
+    "gabber joost",
+    "minecraft is goed",
+    "fortnite niet",
+);
+$nummer = array(
+1,2,3,4,5,6,7,8,9,10
+);
+foreach ($name as $value) {
+    echo "$value" .PHP_EOL;
+}
+
+foreach ($name as $count10){
+    echo "$count10" .PHP_EOL;
+}
+exit();
